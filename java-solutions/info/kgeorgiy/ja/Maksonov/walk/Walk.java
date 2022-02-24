24df1b9e921ac5c1b0dc3fd1cae985e28430ca80
Maksonov_Artem <313353@niuitmo.com>
2022-02-24 16:37:41 +0300

    hm1 fixde

--- java-solutions/info/kgeorgiy/ja/Maksonov/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/Maksonov/walk/Walk.java
@@ -13,14 +13,24 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Walk {
-    private final static String ZERO40_HASH = "0000000000000000000000000000000000000000";
+
+    private final static String ZERO40_HASH = bytesToString(new byte[20]);
     private final static int BUFFER_SIZE = 1024;
 
-    private static String getHash(String fileName) {
-        String result = ZERO40_HASH;
-        byte[] hash = null;
+    private static String bytesToString (byte[] bytes) {
+        if (bytes != null) {
+            StringBuilder sb = new StringBuilder();
+            for (byte el : bytes) {
+                sb.append(String.format("%02x", el));
+            }
+            return sb.toString();
+        }
+        return ZERO40_HASH;
+    }
 
-        try (InputStream file = Files.newInputStream(Paths.get(fileName))) {
+    private static String getHash(Path path, String fileName) throws NoSuchAlgorithmException, IOException, SecurityException {
+        byte[] hash;
+        try (InputStream file = Files.newInputStream(path)) {
             int value;
             byte[] buffer = new byte[BUFFER_SIZE];
             MessageDigest sha;
@@ -30,64 +40,70 @@ public class Walk {
                     sha.update(buffer, 0, value);
                 }
                 hash = sha.digest();
+            } catch (IOException e) {
+                throw new IOException("Error while reading the file " + fileName + " : " + e.getMessage());
             } catch (NoSuchAlgorithmException e) {
-                System.err.println("No such algorithm Exception: " + e.getMessage());
+                throw new NoSuchAlgorithmException("No SHA-1 algorithm" + e.getMessage());
             }
         } catch (IOException e) {
-            System.err.println("Input/Output files error: " + e.getMessage());
+            throw new IOException("Cannot read the file " + fileName);
         } catch (SecurityException e) {
-            System.err.println("Files security error: " + e.getMessage());
+            throw new SecurityException("Forbidden to read the file " + fileName + " : " + e.getMessage());
         }
 
-        if (hash != null) {
-            StringBuilder sb = new StringBuilder();
-            for (byte el : hash) {
-                sb.append(String.format("%02x", el));
-            }
-            result = sb.toString();
-        }
-
-        return result;
+        return bytesToString(hash);
     }
 
     public static void main (String[] args) {
         if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
-            System.err.println("Invalid arguments. Expected 2 not-null arguments.");
+            System.err.println("Invalid arguments. Expected 2 not-null arguments: <input file> <output file>.");
+            return;
+        }
+
+        Path input, output;
+        try {
+            input = Paths.get(args[0]);
+            output = Paths.get(args[1]);
+        } catch (InvalidPathException e) {
+            System.err.println("Invalid path in argument files: " + e.getMessage());
             return;
         }
 
         try {
-            Path input = Paths.get(args[0]);
-            Path output = Paths.get(args[1]);
-
             if (output.getParent() != null) {
-                try {
-                    Files.createDirectories(output.getParent());
-                } catch (IOException | SecurityException e) {
-                    System.err.println("Cannot create output file: " + e.getMessage());
-                    return;
-                }
+                Files.createDirectories(output.getParent());
             }
+        } catch (IOException e) {
+            System.err.println("Cannot create output file: " + e.getMessage());
+            return;
+        } catch (SecurityException e) {
+            System.err.println("Forbidden to create output file: " + e.getMessage());
+            return;
+        }
 
-            try (BufferedReader reader = Files.newBufferedReader(input, StandardCharsets.UTF_8);
-                 BufferedWriter writer = Files.newBufferedWriter(output, StandardCharsets.UTF_8)) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    try {
-                        Path path = Paths.get(line);
-                        writer.write(String.format("%s %s", getHash(path.toString()), line));
-                    } catch (InvalidPathException e) {
-                        writer.write(String.format("%s %s", ZERO40_HASH, line));
-                    }
-                    writer.newLine();
+        try (BufferedReader reader = Files.newBufferedReader(input, StandardCharsets.UTF_8);
+             BufferedWriter writer = Files.newBufferedWriter(output, StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    Path path = Paths.get(line);
+                    writer.write(String.format("%s %s", getHash(path, line), line));
+                } catch (IOException e) {
+                    System.err.println("Error while writing to file: " + e.getMessage());
+                    writer.write(String.format("%s %s", ZERO40_HASH, line));
+                } catch (InvalidPathException ignored) {
+                    writer.write(String.format("%s %s", ZERO40_HASH, line));
+                } catch (SecurityException | NoSuchAlgorithmException e) {
+                    System.err.println(e.getMessage());
+                    writer.write(String.format("%s %s", ZERO40_HASH, line));
                 }
-            } catch (IOException e) {
-                System.err.println("Input/Output files error: " + e.getMessage());
-            } catch (SecurityException e) {
-                System.err.println("Files security error: " + e.getMessage());
+                writer.newLine();
             }
-        } catch (InvalidPathException e) {
-            System.err.println("Invalid argument files: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Cannot open Input/Output files error: " + e.getMessage());
+        } catch (SecurityException e) {
+            System.err.println("Forbidden to read/write files: " + e.getMessage());
         }
     }
 }
+
